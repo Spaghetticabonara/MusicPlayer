ded71a89f74fc1bba72f995298cd21d817ea01d7
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2021-04-15 13:53:16 +0700

    can play&pause

--- app/src/main/java/com/example/musicplayer/PlayerActivity.java
+++ app/src/main/java/com/example/musicplayer/PlayerActivity.java
@@ -1,13 +1,35 @@
 package com.example.musicplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -17,8 +39,8 @@ public class PlayerActivity extends AppCompatActivity {
     Button btnplay, btnnext, btnprev;
     TextView txtsname, txtstart, txtstop;
     SeekBar seekmusic;
-
     String sname;
+
     public static final String EXTRA_NAME = "song_name";
     static MediaPlayer mediaPlayer;
     int position;
@@ -33,7 +55,7 @@ public class PlayerActivity extends AppCompatActivity {
         btnnext = findViewById(R.id.nextbtn);
         btnplay = findViewById(R.id.playbtn);
 
-        txtsname = findViewById(R.id.songname);
+        txtsname = findViewById(R.id.nametxt);
         txtstart = findViewById(R.id.txtstart);
         txtstop = findViewById(R.id.txtend);
 
@@ -41,6 +63,37 @@ public class PlayerActivity extends AppCompatActivity {
 
         if (mediaPlayer != null){
             mediaPlayer.stop();
+            mediaPlayer.release();
         }
+
+        Intent i = getIntent();
+        Bundle bundle = i.getExtras();
+
+        mysongs = (ArrayList) bundle.getParcelableArrayList("songs");
+        String songName = i.getStringExtra("songname");
+        position = bundle.getInt("pos", 0);
+        txtsname.setSelected(true);
+        Uri uri = Uri.parse(mysongs.get(position).toString());
+        sname = mysongs.get(position).getName().toString().replace(".mp3", "");
+        txtsname.setText(sname);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+        mediaPlayer.start();
+
+        btnplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()){
+                    btnplay.setBackgroundResource(R.drawable.ic_play);
+                    mediaPlayer.pause();
+                }
+                else {
+                    btnplay.setBackgroundResource(R.drawable.ic_pause);
+                    mediaPlayer.start();
+                }
+            }
+        });
+
     }
+
 }
